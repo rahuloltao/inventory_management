3044397b2715264d6493b74df71396ee7d4f5042
rahuloltao <rahuloltao@gmail.com>
2025-08-28 11:40:59 +0530

    gg

--- Frontend/src/utils/baseurl.js
+++ Frontend/src/utils/baseurl.js
@@ -1,2 +1,7 @@
-const baseUrl = `${process.env.REACT_APP_BACKEND_URL}/api`;
+const baseUrl = process.env.REACT_APP_BACKEND_URL
+  ? `${process.env.REACT_APP_BACKEND_URL}/api`
+  : "undefined";
+
+console.log("🔎 Base URL being used:", baseUrl);
+
 export default baseUrl;
