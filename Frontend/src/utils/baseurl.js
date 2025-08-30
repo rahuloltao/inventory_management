0b70ef340f22bd85b2b5a2bd9b63185be9ced654
rahuloltao <rahuloltao@gmail.com>
2025-08-30 13:52:55 +0530

    baseurl

--- Frontend/src/utils/baseurl.js
+++ Frontend/src/utils/baseurl.js
@@ -1,6 +1,13 @@
-const baseUrl = process.env.REACT_APP_BACKEND_URL
-  ? `${process.env.REACT_APP_BACKEND_URL}/api`
-  : "undefined";
+// const baseUrl = process.env.REACT_APP_BACKEND_URL
+//   ? `${process.env.REACT_APP_BACKEND_URL}/api`
+//   : "undefined";
+
+// console.log("🔎 Base URL being used:", baseUrl);
+
+// export default baseUrl;
+
+const baseUrl =
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:8000/api";
 
 console.log("🔎 Base URL being used:", baseUrl);
 
